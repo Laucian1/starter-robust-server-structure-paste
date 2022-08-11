bbe4f762c1873f0932c954edae1067af46fb1b68
Russell Smith <resmithjr88@gmail.com>
2022-08-10 22:51:53 -0600

    Organizing Express Code complete

--- src/app.js
+++ src/app.js
@@ -1,57 +1,11 @@
 const express = require("express");
 const app = express();
-
+const pastesRouter = require("./pastes/pastes.router")
 const pastes = require("./data/pastes-data")
 
 app.use(express.json())
 
-app.use("/pastes/:pasteId", (req, res, next) => {
-  const { pasteId } = req.params
-  const foundPaste = pastes.find((paste) => paste.id === Number(pasteId))
-
-  if (foundPaste) {
-    res.json({ data: foundPaste })
-  } else {
-    next({ status: 404, message: `Paste id not found: ${pasteId}` })
-  }
-})
-
-app.get("/pastes", (req, res) => {
-  res.json({ data: pastes })
-})
-
-//New middleware function to validate the request body
-function bodyHasTextProperty(req, res, next) {
-  const { data: { text } = {} } = req.body
-  if (text) {
-    return next() 
-  }
-  next({
-    status: 400,
-    message: "A 'text' property is required."
-  })
-}
-let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0)
-
-app.post(
-  "/pastes", 
-  bodyHasTextProperty, // Add validation middleware function
-  (req, res) => {
-    // Route handler no longer has validation code.
-    const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body
-    const newPaste = {
-      id: ++lastPasteId, // Increment last ID, then assign as the current ID
-      name,
-      syntax,
-      exposure,
-      expiration,
-      text,
-      user_id,
-    }
-    pastes.push(newPaste)
-    res.status(201).json({ data: newPaste })
-  }
-)
+app.use("/pastes", pastesRouter)
 
 // Not found handler
 app.use((request, response, next) => {
